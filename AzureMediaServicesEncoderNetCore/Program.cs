4e869f1dec39eb4f5f56417ce7e088ab130a9e96
Jean-Marc Skopek <jskopek@gmail.com>
2018-04-22 17:19:10 -0700

    Adding hard-coded configuration settings; bad idea in practice!

--- AzureMediaServicesEncoderNetCore/Program.cs
+++ AzureMediaServicesEncoderNetCore/Program.cs
@@ -6,7 +6,13 @@ namespace AzureMediaServicesEncoderNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this is a big no-no in practice! never hard-code sensitive data or commit it to a repository
+            // a better approach would be to use a ConfigurationBuilder instance: 
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.1&tabs=basicconfiguration
+            string tenantDomain = "-REPLACE ME-";
+            string restApiUrl = "-REPLACE ME-";
+            string clientId = "-REPLACE ME-";
+            string clientSecret = "-REPLACE ME-";
         }
     }
 }
